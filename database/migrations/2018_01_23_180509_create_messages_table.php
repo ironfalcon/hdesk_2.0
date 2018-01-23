5dec65ffb111c20c3d98d5496f637f77f23cc370
ironfalcon <ironfalcon@yandex.ru>
2018-01-23 22:17:31 +0400

    create Controller, model and migration Messeges

---
+++ database/migrations/2018_01_23_180509_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('body');
+            $table->integer('from_user_id');
+            $table->integer('to_user_id');
+            $table->boolean('unread')->default(0);
+            $table->string('date_send')->default(date("Y-m-d H:i:s"));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
