5e1792a86b41d02b0dc5505046e743d3752aa55f
ironfalcon <ironfalcon@yandex.ru>
2018-01-26 04:38:43 +0400

    fix navbar and css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,13 +12,19 @@
 
     <!-- Styles -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" rel="stylesheet">
-     
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
 
-   <style>
+<style>
+body{
+    padding-top: 70px;
+    padding-bottom: 30px;
+}
 form{
     display:inline;
 }
+/* Style for news card*/
 .card{
   height: 300px;
   width: 200px;
@@ -49,11 +55,76 @@ form{
     margin-left: 10px;
 }
 
+/*Style for schedule card*/
+
+.schedule-card{
+    height: 200px;
+    width: 200px;
+    border-radius: 5px;
+    background-color: #E1F5FE;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    float: left;
+    margin-left: 20px;
+    margin-top: 20px;
+}
+.schedule-card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.schedule-content{
+    font-family: 'Roboto', sans-serif;
+    vertical-align: middle;
+    text-align: center;
+    margin: 13px;
+    height: fit-content;
+
+}
+.schedule-head{
+    background-color: #80d8ff;
+    border-radius: 5px 5px 0 0;
+    padding: 5px 7px;
+    text-align: center;
+}
+
+/*Style for schedule-today card*/
+
+.schedule-card-today{
+    height: 200px;
+    width: 200px;
+    border-radius: 5px;
+    background-color: #A5D6A7;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    float: left;
+    margin-left: 20px;
+    margin-top: 20px;
+}
+.schedule-card-today:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.schedule-content-today{
+    font-family: 'Roboto', sans-serif;
+    vertical-align: middle;
+    text-align: center;
+    margin: 13px;
+    height: fit-content;
+
+}
+.schedule-head-today{
+    background-color: #4CAF50;
+    border-radius: 5px 5px 0 0;
+    padding: 5px 7px;
+    text-align: center;
+}
+
+
 </style>
 </head>
 <body style="background-color: #ecf0f1;">
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top" style="background-color: #3498db;">
+        <nav class="navbar navbar-default navbar-fixed-top" style="background-color: #3498db;">
             <div class="container">
                 <div class="navbar-header">
 
