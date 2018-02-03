5344de511fa0f3b22983f04ec7b5113663ee8d55
ironfalcon <ironfalcon@yandex.ru>
2018-02-03 23:19:23 +0400

    fix

--- database/migrations/2018_01_23_180509_create_messages_table.php
+++ database/migrations/2018_01_23_180509_create_messages_table.php
@@ -17,7 +17,8 @@ class CreateMessagesTable extends Migration
             $table->increments('id');
             $table->string('body');
             $table->integer('from_user_id');
-            $table->integer('to_user_id');
+            $table->integer('to_user_id')->nullable();
+            $table->integer('to_group_id')->nullable();
             $table->boolean('unread')->default(1);
             $table->string('date_send')->default(date("Y-m-d H:i:s"));
             $table->timestamps();
--- database/migrations/2018_01_25_132358_create_schedules_table.php
+++ database/migrations/2018_01_25_132358_create_schedules_table.php
@@ -17,7 +17,7 @@ class CreateSchedulesTable extends Migration
             $table->increments('id');
             $table->string('day')->nullable();
             $table->string('day_num')->nullable();
-            $table->string('group_name')->nullable();;
+            $table->string('group_name')->nullable();
             $table->string('pair_num')->nullable();
             $table->string('pair_time')->nullable();
             $table->string('aud')->nullable();
